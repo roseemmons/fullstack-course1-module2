e9b0dde8b77f1a2d2d56e5cde97e4e2cb99b50cd
Rose Emmons <rose.emmons@mac.com>
2016-08-14 19:16:23 -0600

    All tests passed.

--- assignment/module2_assignment.rb
+++ assignment/module2_assignment.rb
@@ -2,7 +2,7 @@ class LineAnalyzer
   attr_reader :content, :line_number, :highest_wf_count, :highest_wf_words
 
   def initialize(content, line_number)
-    @content = content # This is a really really really cool cool you you you
+    @content = content
     @line_number = line_number
     @highest_wf_count = 0
     @highest_wf_words = []
@@ -11,16 +11,16 @@ class LineAnalyzer
 
   def calculate_word_frequency
     result = []
-    split_line = @content.split # ["This", "is", "a", "really", "really", "really", "cool", "cool", "you", "you", "you"]
+    split_line = @content.split
 
     split_line.each_index do |index|
       result.push(["#{split_line[index]}", split_line.select { |w| w == split_line[index] }.count] )
     end
 
-    result.uniq! # [["This", 1], ["is", 1], ["a", 1], ["really", 3], ["cool", 2], ["you", 3]]
-    result_hash = result.to_h # {"This"=>1, "is"=>1, "a"=>1, "really"=>3, "cool"=>2, "you"=>3}
+    result.uniq!
+    result_hash = result.to_h
     result_hash.each do |k, v|
-      @highest_wf_words.push(k) if v == result_hash.values.max # = ["really", "you"]
+      @highest_wf_words.push(k) if v == result_hash.values.max
       @highest_wf_count = result_hash.values.max # = 3
     end
   end
@@ -43,25 +43,15 @@ class Solution
       File.readlines(text_file).each_with_index do |line, index|
         clean_line = line.chomp.downcase!
         @analyzers.push(LineAnalyzer.new(clean_line, index))
+        @analyzers.reverse!
       end
     end
   end
 
   def calculate_line_with_highest_frequency
-# Calculate the maximum value for highest_wf_count contained by the LineAnalyzer objects in analyzers array
-# Store this result in the highest_count_across_lines attribute
-# Identifies the LineAnalyzer objects in the analyzers array that have highest_wf_count equal to highest_count_across_lines
-#  attribute value determined previously and stores them in highest_count_words_across_lines.
-    @highest_count_words_across_lines = []
-    @highest_count_across_lines = @analyzers.max_by {|o| o.highest_wf_count}.highest_wf_count.to_i
-
-    @analyzers.each do |o|
-      @highest_count_words_across_lines.push(o.highest_wf_words).flatten! if o.highest_wf_count == @highest_count_across_lines #4
-    end
-    @highest_count_words_across_lines.rotate!
-    # @highest_count_words_across_lines
-    # p @highest_count_across_lines
-  end
+    @highest_count_across_lines = @analyzers.collect { |o| o.highest_wf_count }.max
+    @highest_count_words_across_lines = @analyzers.reject{|o| o.highest_wf_count != @highest_count_across_lines}
+ end
 
   def print_highest_word_frequency_across_lines
     # The following words have the highest word frequency per line:
